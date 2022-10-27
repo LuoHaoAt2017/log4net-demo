92d5d843d8b141626a5721f590055772fad10ca6
洛神赋 <hluo5206@gmail.com>
2022-10-27 08:46:45 +0800

    feat: log4net

---
+++ DotnetLogger/Common/LoggerHelper.cs
@@ -0,0 +1,26 @@
+﻿using log4net;
+
+namespace DotnetLogger.Common
+{
+	internal class LoggerHelper
+	{
+		private static readonly ILog logError = LogManager.GetLogger("LogError");
+		private static readonly ILog logInfo = LogManager.GetLogger("LogInfo");
+
+		public static void Info(string mesg)
+		{
+			if (logInfo.IsInfoEnabled)
+			{
+				logInfo.Info(mesg);
+			}
+		}
+
+		public static void Error(string mesg)
+		{
+			if (logError.IsErrorEnabled)
+			{
+				logError.Error(mesg);
+			}
+		}
+	}
+}
---
+++ DotnetLogger/MyApp.cs
@@ -0,0 +1,15 @@
+﻿using DotnetLogger.Common;
+using System.Windows.Forms;
+
+namespace DotnetLogger
+{
+	public partial class MyApp : Form
+	{
+		public MyApp()
+		{
+			InitializeComponent();
+			LoggerHelper.Info("log info");
+			LoggerHelper.Error("log error");
+		}
+	}
+}
